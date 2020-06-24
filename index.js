0dda2a637b1d9c51d3b8b2e7e8cbc1f06188e842
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-06-24 23:40:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 // Write your solution here!
-const append =['Millo','Otis','Garfield'];
-const prepend=['Millo','Otis','Garfield'];
-const removeLast=['Millo','Otis','Garfield'];
-const removeFirst=['Millo','Otis','Garfield'];
+const pet =['Millo','Otis','Garfield'];
 
 function append(){
 append.push('Odie');
