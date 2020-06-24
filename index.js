2cdaa3902d436aef9301082ee070788e6a99d0e5
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-06-24 23:33:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const prepend=["Millo","Otis","Garfield"];
 const removeLast=["Millo","Otis","Garfield"];
 const removeFirst=["Millo","Otis","Garfield"];
 
-append.ush("Odie");
+append.push(Odie");
 prepend.unshift("Odie");
 removeLast.pop;
 removeFirst.shift;
